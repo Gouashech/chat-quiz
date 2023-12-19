de4a524741fd61cb27e0198229bad5ccd81e7bbc
Gouashech <149411918+Gouashech@users.noreply.github.com>
2023-12-19 17:14:03 +0200

    final

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page d-flex align-items-center justify-content-center vh-100">
+      <div className="text-center">
+        <div className="">
+          <div className="text-center mb-4">
+            <h1>Welcome to the Quiz</h1>
+          </div>
+          <div className="text-center">
+            <Link to="/quiz">
+              <button className="btn btn-primary">Start Quiz</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
